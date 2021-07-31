60dd48e3f74512769b3face9e0b4dd8843279bb8
ewudes <ewudes@gmail.com>
2021-07-31 13:44:05 +0400

    add mocks

---
+++ src/mock/movie.js
@@ -0,0 +1,7 @@
+const TITLES = [`The Dance of Life`, `Sagebrush Trail`, `The Man with the Golden Arm`, `Santa Claus Conquers the Martians`, `Popeye the Sailor Meets Sindbad the Sailor`, `The Great Flamarion`, `Made for Each Other`];
+const DIRECTOR = [`Anthony Mann`, `Anne Wigton`, `Heinz Herald`, `Richard Weil`, `Erich von Stroheim`];
+const ACTORS = [`Erich von Stroheim`, `Mary Beth Hughes`, `Dan Duryea`];
+const WRITES = [`Anne Wigton`, `Heinz Herald`, `Richard Weil`];
+const POSTERS = [`made-for-each-other.png`, `popeye-meets-sinbad.png`, `sagebrush-trail.jpg`, `santa-claus-conquers-the-martians.jpg`, `the-dance-of-life.jpg`, `the-great-flamarion.jpg`, `the-man-with-the-golden-arm.jpg`];
+const COUNTRY = [`USA`, `Thailand`, `England`, `France`];
+const AGE = [`6+`, `12+`, `16+`, `18+`];
